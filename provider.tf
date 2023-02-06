e6e02089634f0a5bf93885111a45189ba425f0d3
Helened22 <blue2015heaven@gmail.com>
2023-02-05 19:49:55 -0600

    configure remote state

--- provider.tf
+++ provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 4.0"
     }
   }
+
+  backend "s3" {
+    bucket = "terr-remote-state-2023"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-remote-state"
+  }
 }
 
 provider "aws" {
